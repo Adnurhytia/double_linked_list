eb685256f6e4359a63ea512fd7b6fc608dcbff03
Adnurhytia <adindanurhayati30@gmail.com>
2022-11-18 17:03:53 +0700

    Menambahkan method if untuk mengecheck data yg dimasukkan

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -15,4 +15,40 @@ namespace double_linked_list
         //point to the preceeding node
         public Node prev;
     }
+    class DoubleLinkedList
+    {
+        Node START;
+
+        //Constructor
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //Check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START!=null)
+                    START.prev = newNode;
+                newNode.next = null;
+                START = newNode;
+                return;
+            }
+        }
+        /*On the execution of the above for loop, prev and 
+         * Current will point to those nodes
+         * between which the new node is to be inserted*/
+    }
 }
