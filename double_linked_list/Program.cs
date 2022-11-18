325a895af8bf4573609e1d1bf0d4f88b3d58af10
Adnurhytia <adindanurhayati30@gmail.com>
2022-11-18 16:52:16 +0700

    Menjabarkan var noMhs,name

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -2,5 +2,17 @@
 
 namespace double_linked_list
 {
-    
+    class Node
+    {
+        /*Node class representn the node of doubly linked list
+         * It consist of the information part and links to
+         * Its succeding and preceeding
+         * In terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
+    }
 }
